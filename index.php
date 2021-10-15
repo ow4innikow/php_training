329067488ef5bba23392c084b916f972d4e823a2
Eugene <ow4innikow@gmail.com>
2021-10-15 22:58:19 +0300

    на сегодня все

--- index.php
+++ index.php
@@ -8,11 +8,24 @@
 <body>
     <?php
 
-    $var = 5; // про кавычки
-    echo "Hello wolrld - $var"; //Hello wolrld - 5
-    echo "Hello wolrld - \$var"; //Hello wolrld - $var
-    echo "Hello wolrld - \"$var\""; //Hello wolrld - "5"  //экранирование
-    echo "Hello wolrld - {$var}3"; //Hello wolrld - 53
+//
+
+    $var = NULL; // 0.0  0 "0" '' NULL - это все будет false
+    if($var){
+        echo 'переменная $var рассматривается как true';
+    }
+    else {
+        echo 'переменная $var рассматривается как false'; // эта часть сработала
+    }
+
+    $var = 'g' ; // все другое содержимое -  ТРУЕ
+    if($var){
+        echo 'переменная $var рассматривается как true';
+    }
+    else {
+        echo 'переменная $var рассматривается как false';
+    }
+
 
 
     ?>
@@ -26,6 +39,42 @@
  *
  *
  *
+// преобразование к числу
+    $str = '45.2';
+    $number = $str - 12;
+    echo $number; // 33.2
+ *
+ //проверка на целочисленность
+      if(is_int(12.5))
+        echo 'это интежер';
+      else
+          echo 'это НЕ интежер';
+ *
+  // gettype() проверяет тип переменной
+    echo gettype(123); // integer
+    echo gettype("123"); // string
+    echo gettype(1.15); // double
+    echo gettype(true); // boolean
+ *
+  $str = 'в';
+    if(empty($str)) // empty() проверяет есть ли что то в строке
+        echo 'строка пустая';
+    else
+        echo 'в строке что то есть';
+ *
+ $user = 'Ivan';
+        if(isset($user)) // isset() проверяет инициализированна ли переменная
+            echo 'переменная существует';
+        else
+            echo 'переменная НЕ существует';
+ *
+   $var = 5; // про кавычки
+    echo "Hello wolrld - $var"; //Hello wolrld - 5
+    echo "Hello wolrld - \$var"; //Hello wolrld - $var
+    echo "Hello wolrld - \"$var\""; //Hello wolrld - "5"  //экранирование
+    echo "Hello wolrld - {$var}3"; //Hello wolrld - 53
+    echo "Hello wolrld - {$var} 3"; //Hello wolrld - 5 3
+    echo `dir`; // можно управлять командами из операциооной системы
  *
  *типы данных в пхп
                 boolean
@@ -38,7 +87,7 @@
                 resourse
                 NULL
  *
- echo 'Главный скрипт<br>'; // подключать файлы include or require
+ echo 'Главный скрипт<br>'; // подключать файлы
         include 'second.php'; // require 'second.php';
         echo 'Главный скрипт<br>';
  *

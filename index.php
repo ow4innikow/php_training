92122db5ebced8230d609837c66e86f0bb7798cf
Eugene <ow4innikow@gmail.com>
2021-10-15 19:44:31 +0300

    some code

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= 'Новая страница'?></title>
+</head>
+<body>
+    <?php
+
+    ?>
+</body>
+</html>
+
+
 <?php
+/*
+
+ *
+
+ *
+    echo 9 + 3 - 4;
+ *
+    echo 'текущая дата и время ';
+    echo date(DATE_RSS);
+ *
+    <title><?= 'новая страница'?></title>
+ *
+    echo  'Hello World';
+ */
+
+
+
